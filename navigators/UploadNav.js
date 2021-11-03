a342761b9c8cca7cd77208d92242400534494536
Mun <xmun74@naver.com>
2021-11-03 14:57:49 +0900

    #17.2 Select Photo 1 (Select header style)

--- navigators/UploadNav.js
+++ navigators/UploadNav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import SelectPhoto from "../screens/SelectPhoto";
 import TakePhoto from "../screens/TakePhoto";
@@ -22,11 +23,27 @@ export default function UploadNav() {
       }}
       tabBarPosition="bottom" //탭 위치 아래로 변경
     >
-      <Tab.Screen name="Select">
+      <Tab.Screen name="SelectRoot">
         {() => (
           // SelectPhoto는 사진선택하는 탭이 있어야 해서 Stack리턴하는 함수 이용
-          <Stack.Navigator>
-            <Stack.Screen name="Select" component={SelectPhoto} />
+          <Stack.Navigator
+            screenOptions={{
+              headerTintColor: "white", //헤더글씨색
+              headerBackTitleVisible: false, //전헤더 타이틀제거
+              headerBackImage: ({ headerTintColor }) => (
+                <Ionicons color={"white"} name="close" size={28} />
+              ),
+              headerStyle: {
+                backgroundColor: "black", //헤더배경색
+                shadowOpacity: 0.3, //헤더 밑 border색 투명도조절
+              },
+            }}
+          >
+            <Stack.Screen
+              name="Select"
+              options={{ title: "Choose a Photo" }}
+              component={SelectPhoto}
+            />
           </Stack.Navigator>
         )}
       </Tab.Screen>
