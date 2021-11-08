1e5531eb3e28f8d1e22ef1a6ca1bce38922a2a6e
Mun <xmun74@naver.com>
2021-11-08 13:48:38 +0900

    #17.3 - 17.5 Select Photo 2,3,4(get&requestPermisstion, getAsset, FlatList, chosenPhoto...)

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components/native";
+import {
+  FlatList,
+  Image,
+  TouchableOpacity,
+  useWindowDimensions,
+} from "react-native";
+import { colors } from "../colors";
 
 const Container = styled.View`
   flex: 1;
@@ -14,11 +23,97 @@ const Bottom = styled.View`
   background-color: black;
 `;
 
-export default function SelectPhoto() {
+const ImageContainer = styled.TouchableOpacity``;
+const IconContainer = styled.View`
+  position: absolute;
+  bottom: 5px;
+  right: 0px;
+`;
+const HeaderRightText = styled.Text`
+  color: ${colors.blue};
+  font-size: 16px;
+  font-weight: 600;
+  margin-right: 7px;
+`;
+
+export default function SelectPhoto({ navigation }) {
+  const [ok, setOk] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [chosenPhoto, setChosenPhoto] = useState("");
+  const getPhotos = async () => {
+    const { assets: photos } = await MediaLibrary.getAssetsAsync(); //앨범모두불러오기
+    setPhotos(photos);
+    setChosenPhoto(photos[0]?.uri);
+  };
+  const getPermissions = async () => {
+    const { accessPrivileges, canAskAgain } =
+      await MediaLibrary.getPermissionsAsync();
+    //안드로이드: statue ==="undetermined"로 해야함.accessPrivileges === "none"가 아니라
+    if (accessPrivileges === "none" && canAskAgain) {
+      //유저가 권한요청 거절 and 우리가 요청아직 안했으면 할수있는지
+      const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync(); //카메라접근권한요청하기
+      if (accessPrivileges !== "none") {
+        //권한수락하면 true
+        setOk(true);
+        getPhotos();
+      }
+    } else if (accessPrivileges !== "none") {
+      //이미 권한부여받은 경우도 true
+      setOk(true);
+      getPhotos();
+    }
+  };
+  const HeaderRight = () => (
+    <TouchableOpacity>
+      <HeaderRightText>Next</HeaderRightText>
+    </TouchableOpacity>
+  );
+  useEffect(() => {
+    getPermissions();
+  }, []);
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: HeaderRight,
+    });
+  }, []);
+  const numColumns = 4;
+  const { width } = useWindowDimensions();
+  const choosePhoto = (file) => {
+    setChosenPhoto(uri);
+  };
+  const renderItem = ({ item: photo }) => (
+    <ImageContainer onPress={() => chosenPhoto(photo.uri)}>
+      <Image
+        source={{ uri: photo.uri }}
+        style={{ width: width / numColumns, height: 100 }}
+      />
+      <IconContainer>
+        <Ionicons
+          name="checkmark-circle"
+          size={18}
+          color={photo.uri === chosenPhoto ? colors.blue : "white"}
+        />
+      </IconContainer>
+    </ImageContainer>
+  );
   return (
     <Container>
-      <Top />
-      <Bottom />
+      <Top>
+        {chosenPhoto !== "" ? (
+          <Image
+            source={{ uri: chosenPhoto }}
+            style={{ width, height: "100%" }}
+          />
+        ) : null}
+      </Top>
+      <Bottom>
+        <FlatList
+          data={photos}
+          numColumns={numColumns}
+          keyExtractor={(photo) => photo.id}
+          renderItem={renderItem}
+        />
+      </Bottom>
     </Container>
   );
 }
