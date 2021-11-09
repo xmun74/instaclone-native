ab9dd8b0b1d3ff2fe25f6d5b400ef68d14b001f5
Mun <xmun74@naver.com>
2021-11-09 17:31:53 +0900

    #18.4 Room Screen 2 (message)

--- screens/Room.js
+++ screens/Room.js
@@ -1,8 +1,8 @@
-import { useQuery } from "@apollo/client";
-import gql from "graphql-tag";
-import React from "react";
-import { useEffect } from "react";
-import { View, Text } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import React, { useEffect } from "react";
+import { FlatList, KeyboardAvoidingView } from "react-native";
+import ScreenLayout from "../components/ScreenLayout";
+import styled from "styled-components/native";
 
 const ROOM_QUERY = gql`
   query seeRoom($id: Int!) {
@@ -20,8 +20,26 @@ const ROOM_QUERY = gql`
   }
 `;
 
+const MessageContainer = styled.View``;
+const Author = styled.View``;
+const Avatar = styled.Image``;
+const Username = styled.Text`
+  color: white;
+`;
+const Message = styled.Text`
+  color: white;
+`;
+
+const TextInput = styled.TextInput`
+  margin-bottom: 50px;
+  width: 95%;
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 1000px;
+`;
+
 export default function Room({ route, navigation }) {
-  const { data } = useQuery(ROOM_QUERY, {
+  const { data, loading } = useQuery(ROOM_QUERY, {
     variables: {
       id: route?.params?.id,
     },
@@ -31,10 +49,35 @@ export default function Room({ route, navigation }) {
       title: `${route?.params?.talkingTo?.username}`,
     });
   }, []);
-  console.log(data);
+  const renderItem = ({ item: message }) => (
+    <MessageContainer>
+      <Author>
+        <Avatar source={{ uri: message.user.avatar }} />
+        <Username>{message.user.username}</Username>
+      </Author>
+      <Message>{message.payload}</Message>
+    </MessageContainer>
+  );
   return (
-    <View>
-      <Text>Messages List</Text>
-    </View>
+    <KeyboardAvoidingView
+      styled={{ flex: 1, backgroundColor: "black" }}
+      behavior="height"
+      keyboardVerticalOffset={100}
+    >
+      <ScreenLayout loading={loading}>
+        <FlatList
+          inverted //밑에서부터 메시지시작
+          style={{ width: "100%" }}
+          data={data?.seeRoom?.messages}
+          keyExtractor={(message) => "" + message.id}
+          renderItem={renderItem}
+        />
+        <TextInput
+          placeholder="Write a message..."
+          returnKeyLabel="Send Message"
+          returnKeyType="Send"
+        />
+      </ScreenLayout>
+    </KeyboardAvoidingView>
   );
 }
