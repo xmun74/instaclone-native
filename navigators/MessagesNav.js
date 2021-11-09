f0bd0464410fff5eb981d1e7b58c722df738af2a
Mun <xmun74@naver.com>
2021-11-09 15:24:37 +0900

    #18.1 Rooms Screen 1

--- navigators/MessagesNav.js
+++ navigators/MessagesNav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { createStackNavigator } from "@react-navigation/stack";
 import Rooms from "../screens/Rooms";
 import Room from "../screens/Room";
@@ -7,7 +8,18 @@ const Stack = createStackNavigator();
 
 export default function MessagesNav() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: "white",
+        headerBackTitleVisible: false, //헤더타이틀 삭제
+        headerStyle: {
+          backgroundColor: "black", //헤더배경
+        },
+        headerBackImage: (
+          { tintColor } //밑화살표로 헤더전페이지에 아이콘변경
+        ) => <Ionicons color={tintColor} name="chevron-down" size={28} />,
+      }}
+    >
       <Stack.Screen name="Rooms" component={Rooms} />
       <Stack.Screen name="Room" component={Room} />
     </Stack.Navigator>
--- screens/Rooms.js
+++ screens/Rooms.js
@@ -1,10 +1,46 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import styled from "styled-components/native";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+import { ROOM_FRAGMENT } from "../fragments";
+import ScreenLayout from "../components/ScreenLayout";
+
+const SEE_ROOMS_QUERY = gql`
+  query seeRooms {
+    seeRooms {
+      ...RoomParts
+    }
+  }
+  ${ROOM_FRAGMENT}
+`;
+
+const RoomContainer = styled.View`
+  background-color: black;
+`;
+
+const RoomText = styled.Text`
+  color: white;
+`;
 
 export default function Rooms() {
+  const { data, loading } = useQuery(SEE_ROOMS_QUERY);
+  const renderItem = ({ item: room }) => (
+    <RoomContainer>
+      <RoomText>
+        {room.unreadTotal === "0"
+          ? "Name of the other person"
+          : `${room.unreadTotal} messages.`}
+      </RoomText>
+    </RoomContainer>
+  );
   return (
-    <View>
-      <Text>Rooms List</Text>
-    </View>
+    <ScreenLayout loading={loading}>
+      <FlatList
+        data={data?.seeRooms}
+        keyExtractor={(room) => "" + room.id}
+        renderItem={renderItem}
+      />
+    </ScreenLayout>
   );
 }
